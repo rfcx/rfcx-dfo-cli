5f5659bb18c50ab068e4d82a5af4a9581560d704
Topher <topherwhite@gmail.com>
2019-03-31 21:43:48 -0700

    conditionals for setup and update

--- setup.sh
+++ setup.sh
@@ -28,5 +28,5 @@ REGISTRATION_TOKEN="${REPLY}";
 curl -X POST "https://api.rfcx.org/v1/guardians/register" -H "Content-Type: application/x-www-form-urlencoded" -H "cache-control: no-cache" -H "x-auth-user: register" -H "x-auth-token: $REGISTRATION_TOKEN" -d "guid=$GUID&token=$TOKEN"
 
 
-wget -O "$SCRIPT_DIR/update.sh" https://raw.githubusercontent.com/rfcx/rfcx-dfo-cli/master/update.sh
+wget -q -O "$SCRIPT_DIR/update.sh" https://raw.githubusercontent.com/rfcx/rfcx-dfo-cli/master/update.sh
 chmod a+x "$SCRIPT_DIR/update.sh";
--- update.sh
+++ update.sh
@@ -2,19 +2,35 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
+TMP_DIR="$SCRIPT_DIR/tmp"
+if [ ! -d $TMP_DIR ]; then mkdir -p $TMP_DIR; fi
+
 if [ ! -d "$SCRIPT_DIR/.git" ]; then 
 
 	echo "updating checkin script...";
 
-	wget -O "$SCRIPT_DIR/checkin.sh" https://raw.githubusercontent.com/rfcx/rfcx-dfo-cli/master/checkin.sh
+	EXEC_DOWNLOAD=`wget -q -O "$TMP_DIR/_checkin.sh" https://raw.githubusercontent.com/rfcx/rfcx-dfo-cli/master/checkin.sh`;
+
+	chmod a+x "$SCRIPT_DIR/checkin_.sh";
+
+	NEW_SCRIPT_DIGEST=$(openssl dgst -sha1 "$TMP_DIR/_checkin.sh" | grep 'SHA1(' | cut -d'=' -f 2 | cut -d' ' -f 2)
+	OLD_SCRIPT_DIGEST=$(openssl dgst -sha1 "$SCRIPT_DIR/checkin.sh" | grep 'SHA1(' | cut -d'=' -f 2 | cut -d' ' -f 2)
+
+	if [ "$NEW_SCRIPT_DIGEST" = "$OLD_SCRIPT_DIGEST" ]; then 
+		echo "no need for update — checkin script has not been changed..."
+	else
+
+		if [ -f "$TMP_DIR/_checkin_old.sh" ]; then rm "$TMP_DIR/_checkin_old.sh"; fi
+		mv "$SCRIPT_DIR/checkin.sh" "$TMP_DIR/_checkin_old.sh" && mv "$TMP_DIR/_checkin.sh" "$SCRIPT_DIR/checkin.sh";
+
+		echo "udpate now"
+	fi
 
 else
 
 	echo "not updating because this is a git repository";
 
 fi
-
-chmod a+x "$SCRIPT_DIR/checkin.sh";
 
 PRIVATE_DIR="$SCRIPT_DIR/.private"
 GUID=`cat "$PRIVATE_DIR/guid";`;
