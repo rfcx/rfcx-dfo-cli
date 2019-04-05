e663d3511647b843c9e52e1dc1e8e3721bb40618
Topher <topherwhite@gmail.com>
2019-04-05 19:03:12 -0400

    added exclude

--- inotify_trigger.sh
+++ inotify_trigger.sh
@@ -9,7 +9,7 @@ TARGET_FILETYPE=$2
 if [ -d "$TARGET_DIRECTORY" ]; then 
 
 	inotifywait --event moved_to --format "%w%f" --timeout 60 --exclude "^.+\.(?!.*wav)" "$TARGET_DIRECTORY" #| grep --line-buffered ".$TARGET_FILETYPE$" #| $SCRIPT_DIR/stdin.sh "checkin"
-
+# 
 else
 
 	echo " - "
