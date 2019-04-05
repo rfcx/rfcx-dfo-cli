8181c79782eb9d53934d27b7dff52b733292af42
Topher <topherwhite@gmail.com>
2019-04-05 18:13:49 -0400

    no line buffer

--- inotify_trigger.sh
+++ inotify_trigger.sh
@@ -9,7 +9,7 @@ TARGET_FILETYPE=$2
 if [ -d "$TARGET_DIRECTORY" ]; then 
 
 	inotifywait --event moved_to --format "%w%f" --timeout 60 "$TARGET_DIRECTORY" | grep 'wav$' | $SCRIPT_DIR/stdin.sh "checkin"
-#
+
 else
 
 	echo " - "
