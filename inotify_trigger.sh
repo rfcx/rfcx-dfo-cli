ed4e61f62202c80cefd4e1ab0364f805b13750fd
Topher <topherwhite@gmail.com>
2019-04-05 16:47:57 -0400

    added inotify trigger

---
+++ inotify_trigger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PATH="/bin:/sbin:/usr/bin:/usr/sbin:/opt/usr/bin:/opt/usr/sbin:/usr/local/bin:usr/local/sbin:$PATH"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+TARGET_DIRECTORY=$1
+TARGET_FILETYPE=$2
+
+if [ -d "$TARGET_DIRECTORY" ]; then 
+
+	inotifywait -e moved_to --format "%w%f" "$TARGET_DIRECTORY" | grep --line-buffered ".$TARGET_FILETYPE" | $SCRIPT_DIR/stdin.sh "checkin"
+
+else
+
+	echo " - "
+	echo " - Directory '$TARGET_DIRECTORY' could not be found..."
+	echo " - "
+
+fi
+
