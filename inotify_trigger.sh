d5af65423b08698fbf93fe5d9adef15cd4deb0c6
Topher <topherwhite@gmail.com>
2019-04-05 20:46:09 -0400

    added exclude

--- inotify_trigger.sh
+++ inotify_trigger.sh
@@ -8,11 +8,7 @@ TARGET_FILETYPE=$2
 
 if [ -d "$TARGET_DIRECTORY" ]; then 
 	
-	inotifywait --event moved_to --format "%w%f" --monitor "$TARGET_DIRECTORY" | grep ".$TARGET_FILETYPE" | $SCRIPT_DIR/stdin.sh "checkin" &
-
-	PID=$!
-
-	echo "ID $PID"
+	inotifywait --event moved_to --format "%w%f" --monitor "$TARGET_DIRECTORY" | grep ".$TARGET_FILETYPE" | $SCRIPT_DIR/stdin.sh "checkin"
 
 else
 
