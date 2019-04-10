6ac555b60865473cd021563c4245c40409caaab1
Topher <topherwhite@gmail.com>
2019-04-10 15:30:49 -0700

    verifying queue

--- queue.sh
+++ queue.sh
@@ -8,8 +8,11 @@ GNU_DATE_BIN="date"; if [[ "$OSTYPE" == "darwin"* ]]; then GNU_DATE_BIN="gdate";
 
 FILEPATH_ORIG=$1
 
-if [ -f "$DB_DIR/queue-queued.db" ]; then
+echo " - "
+echo " - File '$FILEPATH_ORIG'..."
 
+if [ -f "$DB_DIR/queue-queued.db" ]; then
+	
 	if [ -f "$FILEPATH_ORIG" ]; then
 
 		QUEUED_AT_EPOCH=$(($($GNU_DATE_BIN '+%s')*1000))
@@ -18,16 +21,21 @@ if [ -f "$DB_DIR/queue-queued.db" ]; then
 
 		VERIFY_QUEUE_ENTRY=$(sqlite3 "$DB_DIR/queue-queued.db" "SELECT queued_at FROM queued WHERE filepath='$FILEPATH_ORIG';";)
 
-		echo "$VERIFY_QUEUE_ENTRY";
+		if [ "$VERIFY_QUEUE_ENTRY" = "$QUEUED_AT_EPOCH" ]; then 
+			echo " - ...was SUCCESSFULLY added to the queue..."
+		else
+			echo " - ...could NOT be added to queue..."
+			echo " - Specific error unknown..."
+		fi
+
 	else
 
-		echo " - File '$FILEPATH_ORIG' could not be found and was not added to queue..."
+		echo " - ...could not be found on the filesystem and was NOT added to queue..."
 	
 	fi
 
 else
-	echo " - "
-	echo " - Database '$DB_DIR/queue-queued.db' could not be found..."
-	echo " - File '$FILEPATH_ORIG' was not queued..."
-	echo " - ."
+	echo " - ...was NOT queued because database '$DB_DIR/queue-queued.db' could not be found"
 fi
+
+echo " - "
