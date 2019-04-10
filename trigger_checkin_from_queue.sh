076db9e556a74de49ca38e7edb4a9b6d70e1c613
Topher <topherwhite@gmail.com>
2019-04-10 15:51:12 -0700

    refactoring and adding queue iteration scriot

--- trigger_checkin_from_queue.sh
+++ trigger_checkin_from_queue.sh
@@ -4,11 +4,13 @@ PATH="/bin:/sbin:/usr/bin:/usr/sbin:/opt/usr/bin:/opt/usr/sbin:/usr/local/bin:us
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 DB_DIR="$SCRIPT_DIR/databases"; if [ ! -d $DB_DIR ]; then mkdir -p $DB_DIR; fi;
 
+ROW_LIMIT=3;
+
 echo " - "
 
 if [ -f "$DB_DIR/queue-queued.db" ]; then
 
-	QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/queue-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC;";)
+	QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/queue-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC LIMIT $ROW_LIMIT;";)
 	
 	while read -r QUEUE_ENTRY; do
 	  echo "... $QUEUE_ENTRY ..."
