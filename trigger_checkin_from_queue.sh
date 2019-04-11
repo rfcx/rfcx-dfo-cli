e81850a6f5216ed8f23b4e8699dc9d29cf852d2a
Topher <topherwhite@gmail.com>
2019-04-10 20:38:06 -0700

    refactoring and adding queue iteration script

--- trigger_checkin_from_queue.sh
+++ trigger_checkin_from_queue.sh
@@ -8,10 +8,10 @@ GNU_DATE_BIN="date"; if [[ "$OSTYPE" == "darwin"* ]]; then GNU_DATE_BIN="gdate";
 
 echo " - "
 
-if [ -f "$DB_DIR/queue-queued.db" ]; then
-	if [ -f "$DB_DIR/queue-sent.db" ]; then
+if [ -f "$DB_DIR/checkins-queued.db" ]; then
+	if [ -f "$DB_DIR/checkins-sent.db" ]; then
 	
-		QUEUE_ENTRY_COUNT=$(($(sqlite3 "$DB_DIR/queue-queued.db" "SELECT COUNT(*) FROM queued;";)+0))
+		QUEUE_ENTRY_COUNT=$(($(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT COUNT(*) FROM queued;";)+0))
 		
 		if [ "$QUEUE_ENTRY_COUNT" -gt "0" ]; then
 
@@ -20,7 +20,7 @@ if [ -f "$DB_DIR/queue-queued.db" ]; then
 			# Only three checkins will be processed at a time...
 			if [ "$QUEUE_ENTRY_COUNT" -gt "3" ]; then QUEUE_ENTRY_COUNT=3; fi
 
-			QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/queue-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC LIMIT $QUEUE_ENTRY_COUNT;";)
+			QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC LIMIT $QUEUE_ENTRY_COUNT;";)
 		
 			echo " - $QUEUE_ENTRY_COUNT item(s) will be processed..."
 
@@ -31,10 +31,10 @@ if [ -f "$DB_DIR/queue-queued.db" ]; then
 
 				if [ -f "$QUEUE_ENTRY" ]; then
 
-					REMOVE_FROM_QUEUED=$(sqlite3 "$DB_DIR/queue-queued.db" "DELETE FROM queued WHERE filepath='$QUEUE_ENTRY';";)
+					REMOVE_FROM_QUEUED=$(sqlite3 "$DB_DIR/checkins-queued.db" "DELETE FROM queued WHERE filepath='$QUEUE_ENTRY';";)
 					QUEUE_ENTRY_FILENAME=$(basename -- "$QUEUE_ENTRY")
 					SENT_AT_EPOCH=$(($($GNU_DATE_BIN '+%s')*1000))
-					ADD_TO_SENT=$(sqlite3 "$DB_DIR/queue-sent.db" "INSERT INTO sent (sent_at, filename) VALUES ($SENT_AT_EPOCH, '$QUEUE_ENTRY_FILENAME');";)
+					ADD_TO_SENT=$(sqlite3 "$DB_DIR/checkins-sent.db" "INSERT INTO sent (sent_at, filename) VALUES ($SENT_AT_EPOCH, '$QUEUE_ENTRY_FILENAME');";)
 
 			  	$SCRIPT_DIR/checkin.sh "$QUEUE_ENTRY"
 
@@ -62,10 +62,10 @@ if [ -f "$DB_DIR/queue-queued.db" ]; then
 			echo " - Queue is currently empty."
 		fi
 	else
-	echo " - Database '$DB_DIR/queue-sent.db' could not be found"
+	echo " - Database '$DB_DIR/checkins-sent.db' could not be found"
 	fi		
 else
-	echo " - Database '$DB_DIR/queue-queued.db' could not be found"
+	echo " - Database '$DB_DIR/checkins-queued.db' could not be found"
 fi
 
 echo " - "
