efbb34d8a1385f3da91723214f2026e27f4e72b9
Topher <topherwhite@gmail.com>
2019-04-10 21:11:41 -0700

    refactoring and adding queue iteration script

--- triggerd.sh
+++ triggerd.sh
@@ -4,17 +4,17 @@ PATH="/bin:/sbin:/usr/bin:/usr/sbin:/opt/usr/bin:/opt/usr/sbin:/usr/local/bin:us
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 LOGS_DIR="$SCRIPT_DIR/logs"; if [ ! -d $LOGS_DIR ]; then mkdir -p $LOGS_DIR; fi;
 
-SCRIPT_EXECUTION_DURATION=15;
-
 TRIGGER_SCRIPT_NAME=$1
-PARAM_1=$2
-PARAM_2=$3
+TIMEOUT=$2
+PARAM_1=$3
+PARAM_2=$4
 # LOG_FILE="$LOGS_DIR/$TRIGGER_SCRIPT_NAME.log"
 
 $SCRIPT_DIR/trigger_$TRIGGER_SCRIPT_NAME.sh "$PARAM_1" "$PARAM_2" &
 PID=$!
 echo " - Sending '$TRIGGER_SCRIPT_NAME' into background. ($PID)"
+echo " - Process will be killed if it runs longer than $TIMEOUT seconds."
 
-sleep $SCRIPT_EXECUTION_DURATION
+sleep $TIMEOUT
 KILL=$(kill -9 $PID)
 
