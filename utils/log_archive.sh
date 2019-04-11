a5ba64d28116a1e10e4ecebdfc0e714fa0034688
Topher <topherwhite@gmail.com>
2019-04-11 08:14:45 -0700

    log file maintenance

--- utils/log_archive.sh
+++ utils/log_archive.sh
@@ -5,5 +5,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 
 LOGS_DIR="$SCRIPT_DIR/logs"; if [ ! -d $LOGS_DIR/archive ]; then mkdir -p $LOGS_DIR/archive; fi;
 
+GNU_STAT_FLAG="-c%s"; if [[ "$OSTYPE" == "darwin"* ]]; then GNU_STAT_FLAG="-f%z"; fi;
+
+echo " - "
 echo " - We archive log files here, if they're getting too big..."
 
+for LOG_FILEPATH in $LOGS_DIR/*.log; do
+
+	LOG_FILESIZE=$(stat $GNU_STAT_FLAG "$LOG_FILEPATH")
+	echo " - $LOG_FILEPATH - $LOG_FILESIZE"
+
+done
