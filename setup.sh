74cf8b1118389ca504b7bdf1a2fa7143a48313f7
Topher <topherwhite@gmail.com>
2019-04-10 22:51:46 -0700

    refactoring and adding queue iteration script

--- setup.sh
+++ setup.sh
@@ -89,7 +89,7 @@ $SCRIPT_DIR/utils/upgrade.sh "utils-crontab"
 if [ -f "$SCRIPT_DIR/utils/crontab.sh" ]; then
 	$SCRIPT_DIR/utils/crontab.sh "update" 20
 	$SCRIPT_DIR/utils/crontab.sh "triggerd" 1 "checkin_from_queue" 60
-	# $SCRIPT_DIR/utils/crontab.sh "triggerd" 1 "queue_from_inotify" 60
+	# $SCRIPT_DIR/utils/crontab.sh "triggerd" 1 "queue_from_inotify" 60 "/home/ec2-user/audio-dir-new/" "wav"
 fi
 
 echo " - "
