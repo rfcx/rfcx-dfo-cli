80aa41019c42705ae3e6791a09a7ed7d63a6eee7
Topher <topherwhite@gmail.com>
2019-04-10 21:49:34 -0700

    refactoring and adding queue iteration script

--- utils/database_init.sh
+++ utils/database_init.sh
@@ -6,22 +6,22 @@ DB_DIR="$SCRIPT_DIR/databases"; if [ ! -d $DB_DIR ]; then mkdir -p $DB_DIR; fi;
 
 TABLE_NAME=$1
 
-QUERY_CREATE="";
+TABLE_CREATE_QUERY="";
 
 if [ "$TABLE_NAME" = "checkins-queued" ]; then 
-	QUERY_CREATE="CREATE TABLE queued(queued_at INTEGER, filepath TEXT UNIQUE, attempts INTEGER)";
+	TABLE_CREATE_QUERY="CREATE TABLE queued(queued_at INTEGER, filepath TEXT UNIQUE, attempts INTEGER)";
 fi
 
 if [ "$TABLE_NAME" = "checkins-sent" ]; then 
-	QUERY_CREATE="CREATE TABLE sent(sent_at INTEGER, filename TEXT)";
+	TABLE_CREATE_QUERY="CREATE TABLE sent(sent_at INTEGER, filename TEXT)";
 fi
 
 if [ "$TABLE_NAME" = "checkins-complete" ]; then 
-	QUERY_CREATE="CREATE TABLE complete(sent_at INTEGER, completed_at INTEGER, filename TEXT, audio_id TEXT, checkin_id TEXT, latency INTEGER)";
+	TABLE_CREATE_QUERY="CREATE TABLE complete(sent_at INTEGER, completed_at INTEGER, filename TEXT, audio_id TEXT, checkin_id TEXT, latency INTEGER)";
 fi
 
 if [ ! -f "$DB_DIR/$TABLE_NAME.db" ]; then 
-	DB_INIT=$(sqlite3 "$DB_DIR/$TABLE_NAME.db" "$QUERY_CREATE";);
+	DB_INIT=$(sqlite3 "$DB_DIR/$TABLE_NAME.db" "$TABLE_CREATE_QUERY";);
 	echo " - Created $DB_DIR/$TABLE_NAME.db database.";
 else
 	echo " - Database '$TABLE_NAME' already exists";
