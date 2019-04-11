f9f68d8c95e6b10b130836aac3b1c0540afa40b7
Topher <topherwhite@gmail.com>
2019-04-11 05:54:58 -0700

    passing datetime parse pattern as parameter

--- trigger_checkin_from_queue.sh
+++ trigger_checkin_from_queue.sh
@@ -10,46 +10,56 @@ echo " - "
 
 FILENAME_TIMESTAMP_FORMAT=$1
 
+MAX_BATCH_COUNT=3
+
 if [ -f "$DB_DIR/checkins-queued.db" ]; then
 	if [ -f "$DB_DIR/checkins-sent.db" ]; then
 	
-		QUEUE_ENTRY_COUNT=$(($(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT COUNT(*) FROM queued;";)+0))
-		
-		if [ "$QUEUE_ENTRY_COUNT" -gt "0" ]; then
+		for i in {1..60}
+		do
 
-			echo " - $QUEUE_ENTRY_COUNT item(s) currently in the queue."
+			QUEUE_ENTRY_COUNT=$(($(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT COUNT(*) FROM queued;";)+0))
+			
+			if [ "$QUEUE_ENTRY_COUNT" -gt "0" ]; then
 
-			# Only 10 checkins will be processed at a time...
-			if [ "$QUEUE_ENTRY_COUNT" -gt "10" ]; then QUEUE_ENTRY_COUNT=10; fi
+				echo " - $QUEUE_ENTRY_COUNT item(s) currently in the queue."
 
-			QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC LIMIT $QUEUE_ENTRY_COUNT;";)
-		
-			echo " - $QUEUE_ENTRY_COUNT item(s) will be processed..."
+				# Only MAX_BATCH_COUNT checkins will be processed at a time...
+				if [ "$QUEUE_ENTRY_COUNT" -gt "$MAX_BATCH_COUNT" ]; then QUEUE_ENTRY_COUNT=$MAX_BATCH_COUNT; fi
 
-			IFS=$'\n'
+				QUEUE_ENTRIES=$(sqlite3 "$DB_DIR/checkins-queued.db" "SELECT filepath FROM queued ORDER BY queued_at ASC LIMIT $QUEUE_ENTRY_COUNT;";)
+			
+				echo " - $QUEUE_ENTRY_COUNT item(s) will be processed..."
 
-			for QUEUE_ENTRY in $QUEUE_ENTRIES
-			do
+				IFS=$'\n'
 
-				if [ -f "$QUEUE_ENTRY" ]; then
+				for QUEUE_ENTRY in $QUEUE_ENTRIES
+				do
 
-					REMOVE_FROM_QUEUED=$(sqlite3 "$DB_DIR/checkins-queued.db" "DELETE FROM queued WHERE filepath='$QUEUE_ENTRY';";)
-					QUEUE_ENTRY_FILENAME=$(basename -- "$QUEUE_ENTRY")
-					SENT_AT_EPOCH=$(($($GNU_DATE_BIN '+%s')*1000))
-					ADD_TO_SENT=$(sqlite3 "$DB_DIR/checkins-sent.db" "INSERT INTO sent (sent_at, filename) VALUES ($SENT_AT_EPOCH, '$QUEUE_ENTRY_FILENAME');";)
+					if [ -f "$QUEUE_ENTRY" ]; then
 
-			  	$SCRIPT_DIR/checkin.sh "$QUEUE_ENTRY" "$FILENAME_TIMESTAMP_FORMAT"
+						REMOVE_FROM_QUEUED=$(sqlite3 "$DB_DIR/checkins-queued.db" "DELETE FROM queued WHERE filepath='$QUEUE_ENTRY';";)
+						QUEUE_ENTRY_FILENAME=$(basename -- "$QUEUE_ENTRY")
+						SENT_AT_EPOCH=$(($($GNU_DATE_BIN '+%s')*1000))
+						ADD_TO_SENT=$(sqlite3 "$DB_DIR/checkins-sent.db" "INSERT INTO sent (sent_at, filename) VALUES ($SENT_AT_EPOCH, '$QUEUE_ENTRY_FILENAME');";)
 
-			  else
+				  	$SCRIPT_DIR/checkin.sh "$QUEUE_ENTRY" "$FILENAME_TIMESTAMP_FORMAT"
 
-			  	echo " - '$QUEUE_ENTRY' could not be found on filesystem..."
+				  else
 
-			  fi
-			done
+				  	echo " - '$QUEUE_ENTRY' could not be found on filesystem..."
 
-		else
-			echo " - Queue is currently empty."
-		fi
+				  fi
+				done
+
+			else
+				echo " - Queue is currently empty."
+			fi
+
+			sleep 2
+
+		done
+
 	else
 	echo " - Database '$DB_DIR/checkins-sent.db' could not be found"
 	fi		
