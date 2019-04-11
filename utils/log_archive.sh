ba0d1c331065571c9cf21f9cae90b62729f9faaa
Topher <topherwhite@gmail.com>
2019-04-11 08:25:52 -0700

    log file maintenance

--- utils/log_archive.sh
+++ utils/log_archive.sh
@@ -5,14 +5,27 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 
 LOGS_DIR="$SCRIPT_DIR/logs"; if [ ! -d $LOGS_DIR/archive ]; then mkdir -p $LOGS_DIR/archive; fi;
 
+GNU_DATE_BIN="date"; if [[ "$OSTYPE" == "darwin"* ]]; then GNU_DATE_BIN="gdate"; fi;
 GNU_STAT_FLAG="-c%s"; if [[ "$OSTYPE" == "darwin"* ]]; then GNU_STAT_FLAG="-f%z"; fi;
 
 echo " - "
-echo " - We archive log files here, if they're getting too big..."
+echo " - Running Logfile maintenance..."
+
+# MAX_FILESIZE_KB=$((5*1024))
+MAX_FILESIZE_KB=$((1*140))
 
 for LOG_FILEPATH in $LOGS_DIR/*.log; do
 
-	LOG_FILESIZE=$(($(($(stat $GNU_STAT_FLAG "$LOG_FILEPATH")))/1024))
-	echo " - $LOG_FILEPATH - $LOG_FILESIZE"
+	LOG_FILESIZE_KB=$(($(($(stat $GNU_STAT_FLAG "$LOG_FILEPATH")))/1024))
+
+	if [ "$LOG_FILESIZE_KB" -gt "$MAX_FILESIZE_KB" ]; then
+
+		LOG_FILENAME=$(basename -- "$LOG_FILEPATH")
+
+		NEW_LOG_FILEPATH="$LOGS_DIR/archive/XXXX-XX-XX-$LOG_FILENAME.log"
+
+		mv $LOG_FILEPATH $NEW_LOG_FILEPATH && touch $LOG_FILEPATH;
+
+	fi
 
 done
