b7c3d133e44173f38d04ddaa6f1689f6d738ab71
Topher <topherwhite@gmail.com>
2019-04-11 17:20:14 -0700

    fixed syntax error in create_credentials

--- utils/setup/create_credentials.sh
+++ utils/setup/create_credentials.sh
@@ -7,20 +7,24 @@ PRIVATE_DIR="$APP_DIR/.private"; if [ ! -d $PRIVATE_DIR ]; then mkdir -p $PRIVAT
 
 
 if [ ! -f "$PRIVATE_DIR/guardian_guid" ]; then
+
 	HEX_CHAR_OPTIONS=abcdef0123456789
 	GUARDIAN_GUID=`for i in {1..12} ; do echo -n "${HEX_CHAR_OPTIONS:RANDOM%${#HEX_CHAR_OPTIONS}:1}"; done;`
 	echo "$GUARDIAN_GUID" > "$PRIVATE_DIR/guardian_guid"
+
 fi 
 
 if [ ! -f "$PRIVATE_DIR/api_token" ]; then
+
 	FULL_CHAR_OPTIONS=abcdefghijklmnopqrstuvwxyz0123456789
 	API_TOKEN=`for i in {1..40} ; do echo -n "${FULL_CHAR_OPTIONS:RANDOM%${#FULL_CHAR_OPTIONS}:1}"; done;`
 	echo "$API_TOKEN" > "$PRIVATE_DIR/api_token"
+
 fi 
 
 if [ ! -f "$PRIVATE_DIR/api_hostname" ]; then 
+
 	API_HOSTNAME="https://api.rfcx.org"
 	echo "$API_HOSTNAME" > "$PRIVATE_DIR/api_hostname"
-else
-
-
+	
+fi
