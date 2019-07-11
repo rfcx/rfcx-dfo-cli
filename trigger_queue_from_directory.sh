01905894ca5c380854b8d14949137cb79b5fd308
Topher <topherwhite@gmail.com>
2019-07-11 16:09:20 +0900

    channel downsampling

--- trigger_queue_from_directory.sh
+++ trigger_queue_from_directory.sh
@@ -10,7 +10,23 @@ if [ -d "$TARGET_DIRECTORY" ]; then
 	
 	REGEX_FILTER=".*\.$TARGET_FILETYPE$"
 
-	# we'd put some directory listing logic here
+	# THIS NEED TO BE OVERHAULED...
+
+	for FILEPATH_ORIG in $TARGET_DIRECTORY/*.$TARGET_FILETYPE; do
+
+		# $APP_DIR/queue.sh $ORIG_FILE
+
+
+		# echo $FILEPATH_ORIG
+		FILENAME_ORIG=$(basename -- "$FILEPATH_ORIG")
+		CODEC_ORIG=$(echo $FILENAME_ORIG | rev | cut -d'.' -f 1 | rev | tr '[:upper:]' '[:lower:]')
+		TIMESTAMP_ORIG=$(echo $FILENAME_ORIG | cut -d'.' -f 1)
+		EPOCH=$(($((16#$TIMESTAMP_ORIG))*1000))
+		echo "$TIMESTAMP_ORIG - $EPOCH"
+		EXEC_AUDIO_CONVERT=$(ffmpeg -loglevel panic -i "$FILEPATH_ORIG" -ar 48000 "$TARGET_DIRECTORY/-flac/$EPOCH.flac")
+
+
+	done
 
 else
 
