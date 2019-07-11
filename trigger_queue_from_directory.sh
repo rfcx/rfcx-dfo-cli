2c62be16743718e486775edc591392edd8396da5
Topher <topherwhite@gmail.com>
2019-07-11 17:59:15 +0900

    queue by directory

--- trigger_queue_from_directory.sh
+++ trigger_queue_from_directory.sh
@@ -7,23 +7,29 @@ TARGET_DIRECTORY=$1
 TARGET_FILETYPE=$2
 
 if [ -d "$TARGET_DIRECTORY" ]; then 
-	
-	REGEX_FILTER=".*\.$TARGET_FILETYPE$"
 
-	# THIS NEED TO BE OVERHAULED...
+
+	
+		REGEX_FILTER=".*\.$TARGET_FILETYPE$"
+
+			# # THIS NEED TO BE OVERHAULED...
 
 	for FILEPATH_ORIG in $TARGET_DIRECTORY/*.$TARGET_FILETYPE; do
 
-		# $APP_DIR/queue.sh $ORIG_FILE
+			# 	# $APP_DIR/queue.sh $ORIG_FILE
 
 
-		# echo $FILEPATH_ORIG
-		FILENAME_ORIG=$(basename -- "$FILEPATH_ORIG")
-		CODEC_ORIG=$(echo $FILENAME_ORIG | rev | cut -d'.' -f 1 | rev | tr '[:upper:]' '[:lower:]')
-		TIMESTAMP_ORIG=$(echo $FILENAME_ORIG | cut -d'.' -f 1)
-		EPOCH=$(($((16#$TIMESTAMP_ORIG))*1000))
-		echo "$TIMESTAMP_ORIG - $EPOCH"
-		EXEC_AUDIO_CONVERT=$(ffmpeg -loglevel panic -i "$FILEPATH_ORIG" -ar 48000 "$TARGET_DIRECTORY/-flac/$EPOCH.flac")
+			FILENAME_ORIG=$(basename -- "$FILEPATH_ORIG")
+
+
+			echo $FILENAME_ORIG
+			$APP_DIR/queue.sh $FILEPATH_ORIG
+
+			# 	# CODEC_ORIG=$(echo $FILENAME_ORIG | rev | cut -d'.' -f 1 | rev | tr '[:upper:]' '[:lower:]')
+			# 	# TIMESTAMP_ORIG=$(echo $FILENAME_ORIG | cut -d'.' -f 1)
+			# 	# EPOCH=$(($((16#$TIMESTAMP_ORIG))*1000))
+			# 	# echo "$TIMESTAMP_ORIG - $EPOCH"
+			# 	# EXEC_AUDIO_CONVERT=$(ffmpeg -loglevel panic -i "$FILEPATH_ORIG" -ar 48000 "$TARGET_DIRECTORY/-flac/$EPOCH.flac")
 
 
 	done
